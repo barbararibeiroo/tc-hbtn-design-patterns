a2404dee45fbf022aa9d133753ee875234508254
barbararibeiroo <barbararibeiro18@gmail.com>
2022-05-05 11:55:42 -0300

    midia_player

--- 0x03/midia_player/VideoMediaPlayer.java
+++ 0x03/midia_player/VideoMediaPlayer.java
@@ -3,11 +3,11 @@ public class VideoMediaPlayer implements AdvancedMediaPlayer {
 
     @Override
     public void reproduzirVlc(String VLC) {
-            System.out.print("Reproduzindo VLC" + VLC);
+            System.out.print("Reproduzindo VLC: " + VLC);
     }
 
     @Override
     public void reproduzirMp4(String MP4) {
-        System.out.print("Reproduzindo VLC" + MP4);
+        System.out.print("Reproduzindo MP4: " + MP4);
     }
 }
